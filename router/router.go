b878ceff919da2f0325fe3376af25e1e56906198
Miguel <miguelalvinlynn@gmail.com>
2026-05-17 18:35:29 -0300

    subindo parte do CRUD

--- router/router.go
+++ router/router.go
@@ -1,13 +1,13 @@
 package router
 
 import (
-	"Api-Aula_1/routes"
+	"Api-Aula_1/router/routes"
 
 	"github.com/gorilla/mux"
 )
 
 func New() *mux.Router {
-	r := mux.NewRouter()
-	routes.Register(r)
-	return r
+	rotas := mux.NewRouter()
+	routes.Register(rotas)
+	return rotas
 }
---
+++ router/routes/routes.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Routes struct {
+	URI      string
+	Method   string
+	Function func(http.ResponseWriter, *http.Request)
+}
+
+func Register(r *mux.Router) {
+	var routes []Routes
+	routes = append(routes, booksRoutes...)
+	routes = append(routes, usersRoutes...)
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+}
